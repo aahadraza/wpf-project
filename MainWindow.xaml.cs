feef0eb6a1720932ce5d4e9f1d05d016645515a2
AHAD RAZA <83767844+aahadraza@users.noreply.github.com>
2021-10-13 11:49:24 -0700

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media.Imaging;
+using Microsoft.Win32;
+
+namespace WpfSavePhotoToOracle
+{
+    /// <summary>  
+    /// Interaction logic for Window1.xaml  
+    /// </summary>  
+    public partial class Window1 : Window
+    {
+        private object imgPhoto;
+
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void btnLoad_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog op = new OpenFileDialog();
+            op.Title = "Select a picture";
+            op.Filter = "All supported graphics|*.jpg;*.jpeg;*.png|" +
+              "JPEG (*.jpg;*.jpeg)|*.jpg;*.jpeg|" +
+              "Portable Network Graphic (*.png)|*.png";
+            if (op.ShowDialog() == true)
+            {
+                imgPhoto = new BitmapImage(new Uri(op.FileName));
+            }
+
+        }
+    }
+}
